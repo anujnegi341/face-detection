a331fca9a0e56885c1a4c5b69597ee21ceaa9f9d
Anuj Negi <negi.anuj341@gmail.com>
2021-01-10 23:17:54 +0530

    box location

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace = data.data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
