df88cbdbd52fd4776f044198e5995011e3cfb19f
Anuj Negi <negi.anuj341@gmail.com>
2021-01-11 00:07:18 +0530

    Updates

---
+++ precache-manifest.a5b30cfd9fdb9ca2b482a9facf14e3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2445593cba6798f37c9bfb407118dd58",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "08bb9701978c8da7b034",
+    "url": "/face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "deab56b3ea6d722bc0e8",
+    "url": "/face-detection/static/css/main.47fc5040.chunk.css"
+  },
+  {
+    "revision": "08bb9701978c8da7b034",
+    "url": "/face-detection/static/js/2.d294531d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detection/static/js/2.d294531d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deab56b3ea6d722bc0e8",
+    "url": "/face-detection/static/js/main.c1eae7e2.chunk.js"
+  },
+  {
+    "revision": "7b37c686924301a8fc73",
+    "url": "/face-detection/static/js/runtime-main.7867445b.js"
+  }
+]);
