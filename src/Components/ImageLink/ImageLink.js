73bbdfd3d157edeb9e489008a4efaf500bcc689b
Anuj Negi <negi.anuj341@gmail.com>
2021-01-25 22:13:01 +0530

    update image component

--- src/Components/ImageLink/ImageLink.js
+++ src/Components/ImageLink/ImageLink.js
@@ -5,11 +5,11 @@ const ImageLink = ({onInputChange, onSubmit}) => {
   return(
     <div>
       <p className='f3'>
-        Here you can detect faces on the photograph. It will not show their name though !!
+        Here you can detect faces on the photograph. Enter image URL for detection !!
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+          <input className='f4 pa2 w-70 center' type='text' placeholder='Enter an image url' onChange={onInputChange} />
           <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onSubmit} > Detect </button>
         </div>
       </div>
